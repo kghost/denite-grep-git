33588d8f7eb530b0676ef2326ae8010b53c1e294
Zang MingJie <zealot0630@gmail.com>
2019-06-24 20:58:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Denite-grep-git
+
+denite source for using git grep command
+
+## Dependencies
+
+- [denite.nvim](https://github.com/Shougo/denite.nvim)
+- Git
+
+## Usage
+
+As same as Denite grep
---
+++ rplugin/python3/denite/source/gitgrep.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from denite.source.grep import Source as Grep
+
+class Source(Grep):
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'grep/git'
+        self.vars = {
+            'command': ['git', '--no-pager', 'grep'],
+            'default_opts': ['-n', '--no-color'],
+            'recursive_opts': [],
+            'pattern_opt': ['-e'],
+            'separator': ['--'],
+            'final_opts': [],
+            'min_interactive_pattern': 3,
+        }
